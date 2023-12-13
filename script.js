98b89e4562d2fbdb464f04c6005ca8faac173ab9
Ben <luben12337@gmail.com>
2023-12-13 20:39:31 +1100

    Refactored the code to use classes

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Keep track of the library of books
-const myLibrary = [];
-
 // define buttons
 const addBook = document.getElementById('addBook');
 const submit = document.getElementById('submitForm');
@@ -10,76 +7,73 @@ const grid = document.querySelector('.grid-container');
 const dialog = document.querySelector('dialog');
 const form = document.querySelector('form');
 
-function Book(title, author, pages, read) {
-    // the constructor
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
 
-function addBookToLibrary(book) {
+class Library {
+    constructor() {
+        this._myLibrary = [];
+    }
 
-    // first push it to the array
-    myLibrary.push(book);
+    addBookToLibrary(book) {
+        this._myLibrary.push(book);
+        this.updateGrid();
+    }
 
-    // loop through the array and add divs
-    updateGrid();
-}
+    updateReadStatus(book) {
+        book.read = book.read ? false : true;
+        this.updateGrid();
+    }
 
-function updateGrid() {
-    // loop through the array
-    grid.innerHTML = "";
-    for (const currentBook of myLibrary) {
-        const bookCard = document.createElement('div');
-        bookCard.innerHTML = `
+    removeBook(book) {
+        for (let i = 0; i < this._myLibrary.length; i++) {
+            if (this._myLibrary[i].title == book.title && this._myLibrary[i].author == book.author) {
+                this._myLibrary.splice(i, 1);
+                break;
+            }
+        }
+        this.updateGrid();
+    }
+
+    get myLibrary() {
+        return this._myLibrary;
+    }
+
+    updateGrid() {
+        grid.innerHTML = "";
+        for (const currentBook of this._myLibrary) {
+            const bookCard = document.createElement('div');
+            bookCard.innerHTML = `
         <h4 id='author'></h4>
         <div id='title-div'><h2 id='title'></h2></div>
         <h4 id='pages'></h4>
         <div><h4 id='readStatus'></h4><img src="trashcan.svg" width="15px" id='delete'></div>
         `;
 
-        bookCard.querySelector('#author').innerText = currentBook.author;
-        bookCard.querySelector('#title').innerText = currentBook.title;
-        bookCard.querySelector('#pages').innerText = currentBook.pages + ' pages';
-        bookCard.querySelector('#readStatus').addEventListener('click', () => {
-            updateReadStatus(currentBook);
-        });
-        bookCard.querySelector('#readStatus').innerText = currentBook.read ? 'Read' : 'Not Read';
-        bookCard.querySelector('#readStatus').className = currentBook.read ? 'read' : 'notRead';
-        bookCard.querySelector('#delete').addEventListener('click', () => {
-            removeBook(currentBook);
-        });
+            bookCard.querySelector('#author').innerText = currentBook.author;
+            bookCard.querySelector('#title').innerText = currentBook.title;
+            bookCard.querySelector('#pages').innerText = currentBook.pages + ' pages';
+            bookCard.querySelector('#readStatus').addEventListener('click', () => {
+                this.updateReadStatus(currentBook);
+            });
+            bookCard.querySelector('#readStatus').innerText = currentBook.read ? 'Read' : 'Not Read';
+            bookCard.querySelector('#readStatus').className = currentBook.read ? 'read' : 'notRead';
+            bookCard.querySelector('#delete').addEventListener('click', () => {
+                this.removeBook(currentBook);
+            });
 
-        bookCard.classList.add('card');
-
-        grid.appendChild(bookCard);
-    }
-}
-
-function updateReadStatus(book) {
-    book.read = book.read ? false : true;
-    updateGrid();
-}
-
-function removeBook(book) {
-    // search through for the book title and pop it from myLibrary
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].title == book.title && myLibrary[i].author == book.author) {
-            myLibrary.splice(i, 1);
-            break;
+            bookCard.classList.add('card');
+            grid.appendChild(bookCard);
         }
     }
-    updateGrid();
 }
 
-addBook.addEventListener('click', () => {
-    dialog.showModal();
-});
-
-cancel.addEventListener('click', () => {
-    dialog.close();
-});
+class Book {
+    constructor(title = "Harry Potter", author = "J.K Rowling", pages = 0, read = false) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
 
 submit.addEventListener('click', (event) => {
     event.preventDefault();
@@ -89,21 +83,28 @@ submit.addEventListener('click', (event) => {
     const author = document.getElementById('bookAuthor').value;
     const pages = document.getElementById('bookPages').value;
     const read = document.querySelector('#radio-read').checked ? true : false;
-    console.log("read:" + read);
 
     // create book
     const newBook = new Book(title, author, pages, read);
-    addBookToLibrary(newBook);
+    testLibrary.addBookToLibrary(newBook);
     dialog.close();
     form.reset();
 });
 
 
-// Test case to add book
-const testBook = new Book("Harry Potter", "J.K. Rowling", 345, true);
-addBookToLibrary(testBook);
-console.log(myLibrary);
+addBook.addEventListener('click', () => {
+    dialog.showModal();
+});
 
+cancel.addEventListener('click', () => {
+    dialog.close();
+});
+
+
+// Test case to add book
+const testLibrary = new Library();
+const testBook = new Book("Harry Potter", "J.K. Rowling", 345, true);
+testLibrary.addBookToLibrary(testBook);
 
 
 
